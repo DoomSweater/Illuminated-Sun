c18228d9e382fb618efc6ef4e07ce4cccd3783ec
Gerald Clark <farthestempire@gmail.com>
2021-01-07 22:38:28 -0600

    Integrated FMOD, Set up simple SoundManager gameobject. Music Plays on start.

---
+++ Illuminated Sun/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager mainAudio;
+
+    public string music;
+
+    FMOD.Studio.EventInstance musicEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //check 
+        if(mainAudio != null)
+        {
+            DestroyImmediate(this);
+        }
+        else
+        {
+            mainAudio = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        musicEvent = FMODUnity.RuntimeManager.CreateInstance(SoundManager.mainAudio.music);
+        musicEvent.start();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
